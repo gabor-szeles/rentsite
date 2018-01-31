c95b26972bb9bb9e109d385a44da54d7edce771b
zoltanpataki <zolee7777@gmail.com>
2018-01-31 11:30:52 +0100

    annotations are done in ItemCategory

--- src/main/java/com/codecool/rentsite/rentable/category/ItemCategory.java
+++ src/main/java/com/codecool/rentsite/rentable/category/ItemCategory.java
@@ -1,6 +1,18 @@
 package com.codecool.rentsite.rentable.category;
 
+import com.codecool.rentsite.rentable.Item;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class ItemCategory extends Category {
+
+    @OneToMany(mappedBy = "itemCategory")
+    private Set<Item> itemSet = new HashSet<>();
+
 
     public ItemCategory(String name) {
         super(name);
