4427c6f34f9e47d64e0011febefbd470cae4a765
Szűcs Tibor <job.szucstibor@gmail.com>
2018-01-31 15:42:25 +0100

    dummy sql updated

--- src/main/resources/sql/dummy_sql.sql
+++ src/main/resources/sql/dummy_sql.sql
@@ -2,8 +2,8 @@ INSERT INTO users VALUES (1, 'matyko@matyimail.com', 'Matyi', 'Gyuli', '12345', 
 INSERT INTO users VALUES (2, 'gyuli', 'Gyuli', 'Matyi', '12345', 'Gyuli');
 INSERT INTO itemcategory VALUES (1, 'Matyi Stuff');
 INSERT INTO item VALUES (1, TRUE, 'thing', 'thing1', '10', 'HUF', 1, 1);
-INSERT INTO review VALUES (1, 'Its great user', 2, 'user', 1, 2);
-INSERT INTO review VALUES (2, 'Its good user', 3, 'user', 2, 1);
-INSERT INTO review VALUES (3, 'Its good item', 4, 'reservation', 2, 1);
-INSERT INTO review VALUES (4, 'Its great item', 5, 'reservation', 2, 1);
-INSERT INTO review VALUES (5, 'Its awesome item', 1, 'reservation', 1, 2);
+INSERT INTO userreview VALUES (1, 'Its great user', 2, 1, 2);
+INSERT INTO userreview VALUES (2, 'Its good user', 3, 2, 1);
+INSERT INTO reservationreview VALUES (3, 'Its good item', 4, 2, 1);
+INSERT INTO reservationreview VALUES (4, 'Its great item', 5, 2, 1);
+INSERT INTO reservationreview VALUES (5, 'Its awesome item', 1, 1, 2);
