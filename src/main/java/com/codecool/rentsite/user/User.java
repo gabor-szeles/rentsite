754f737896b15018830b445a46f09737197c3850
Szűcs Tibor <job.szucstibor@gmail.com>
2018-01-31 09:17:06 +0100

    User fields added with their getters and setters

--- src/main/java/com/codecool/rentsite/user/User.java
+++ src/main/java/com/codecool/rentsite/user/User.java
@@ -10,10 +10,75 @@ public class User {
 
     @Id
     int id;
+    String firstName;
+    String lastName;
+    String username;
+    String password;
+    String email;
 
-    String name;
+    public User(){
+    }
 
-    public User(String name) {
-        this.name = name;
+    public User(String username) {
+        this.username = username;
+    }
+
+    public User(String firstName, String lastName, String username, String password, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    //Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
