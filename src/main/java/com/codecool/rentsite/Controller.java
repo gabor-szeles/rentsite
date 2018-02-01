de93ed4757af24e960c663c099f3511195375c4d
gabor-szeles <szelesgbr@gmail.com>
2018-02-01 15:03:25 +0100

    modified controller method to apply to new userdao class

--- src/main/java/com/codecool/rentsite/Controller.java
+++ src/main/java/com/codecool/rentsite/Controller.java
@@ -1,22 +1,20 @@
 package com.codecool.rentsite;
 
-import com.codecool.rentsite.jdbc.JDBC;
+import com.codecool.rentsite.DAO.UserDao;
 import com.codecool.rentsite.user.User;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
-import javax.jws.soap.SOAPBinding;
 import java.util.*;
 
 public class Controller {
 
-    public static ModelAndView renderProducts(Request req, Response res) {
+    public static ModelAndView renderUsers(Request req, Response res) {
         Map params = new HashMap();
-        JDBC jdbc = JDBC.getInstance();
-        List<User> returnValues = jdbc.executeQuery();
+        UserDao userDao = UserDao.getInstance();
+        List<User> returnValues = userDao.executeQuery();
         params.put("userList", returnValues);
-        System.out.println("HEREEEEEEEEEE: " + returnValues.get(0).getEmail());
         return new ModelAndView(params, "/index");
     }
 }
