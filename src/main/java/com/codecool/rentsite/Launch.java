aa02d1c5d2e923d385359b94cd3cff50dc343b48
János Csjernyik <janos.csjernyik@gmail.com>
2018-02-01 14:28:23 +0100

    query logic removed

--- src/main/java/com/codecool/rentsite/Launch.java
+++ src/main/java/com/codecool/rentsite/Launch.java
@@ -25,17 +25,5 @@ public class Launch {
 
 
         get("/", (Request req, Response res) -> new ThymeleafTemplateEngine().render(Controller.renderProducts(req, res)));
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaexamplePU");
-        EntityManager em = emf.createEntityManager();
-
-
-        String query = "SELECT r FROM ReservationReview r";
-        Query query1 = em.createQuery(query);
-        List<Review> results = query1.getResultList();
-
-        System.out.println(results);
-        em.close();
-        emf.close();
     }
 }
