d826b2f3c5fc02b4ff8d6ea1c05a39515e76af3e
János Csjernyik <janos.csjernyik@gmail.com>
2018-02-01 16:07:51 +0100

    list s created to handel templlates

--- src/main/java/com/codecool/rentsite/Controller.java
+++ src/main/java/com/codecool/rentsite/Controller.java
@@ -1,6 +1,11 @@
 package com.codecool.rentsite;
 
+import com.codecool.rentsite.DAO.RentableDAO;
+import com.codecool.rentsite.DAO.ReservationDAO;
 import com.codecool.rentsite.DAO.UserDao;
+import com.codecool.rentsite.rentable.Item;
+import com.codecool.rentsite.rentable.Service;
+import com.codecool.rentsite.reservation.Reservation;
 import com.codecool.rentsite.user.User;
 import spark.ModelAndView;
 import spark.Request;
@@ -13,8 +18,18 @@ public class Controller {
     public static ModelAndView renderUsers(Request req, Response res) {
         Map params = new HashMap();
         UserDao userDao = UserDao.getInstance();
+        RentableDAO rentableDAO = RentableDAO.getInstance();
+        ReservationDAO reservationDAO = ReservationDAO.getInstance();
+
+        List<Item> items = rentableDAO.executeItemQuery();
+        List<Service> services = rentableDAO.executeServiceQuery();
+        List<Reservation> reservations = reservationDAO.executeQuery();
         List<User> returnValues = userDao.executeQuery();
+
         params.put("userList", returnValues);
+        params.put("itemList", items);
+        params.put("serviceList", services);
+        params.put("reservationList", reservations);
         return new ModelAndView(params, "/index");
     }
 }
