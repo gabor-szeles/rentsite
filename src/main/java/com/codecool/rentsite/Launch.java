1e773b56f207c3b20729803b7d6c08b8da8110a7
Szűcs Tibor <job.szucstibor@gmail.com>
2018-02-01 12:53:22 +0100

    Basic spark server added to the Launch class

--- src/main/java/com/codecool/rentsite/Launch.java
+++ src/main/java/com/codecool/rentsite/Launch.java
@@ -2,23 +2,35 @@ package com.codecool.rentsite;
 
 
 import com.codecool.rentsite.review.Review;
+import org.apache.log4j.BasicConfigurator;
+import spark.Request;
+import spark.Response;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 
 import javax.persistence.*;
 
 import java.util.List;
 
+import static spark.Spark.*;
+
 public class Launch {
 
     public static void main(String[] args) {
+        exception(Exception.class, (e, req, res) -> e.printStackTrace());
+        staticFileLocation("/public");
+        port(8888);
+        BasicConfigurator.configure();
+
+
+
+        get("/", (Request req, Response res) -> new ThymeleafTemplateEngine().render(Controller.renderProducts(req, res)));
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaexamplePU");
         EntityManager em = emf.createEntityManager();
 
 
         String query = "SELECT r FROM ReservationReview r";
-
-
-
         Query query1 = em.createQuery(query);
         List<Review> results = query1.getResultList();
 
