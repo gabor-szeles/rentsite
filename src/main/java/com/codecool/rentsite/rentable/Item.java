500d114e7247f6e7361703e9aa3e83ed687655af
János Csjernyik <janos.csjernyik@gmail.com>
2018-02-01 16:10:47 +0100

    item query created

--- src/main/java/com/codecool/rentsite/rentable/Item.java
+++ src/main/java/com/codecool/rentsite/rentable/Item.java
@@ -6,8 +6,10 @@ import com.codecool.rentsite.user.User;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 @Entity
+@NamedQueries(@NamedQuery(name = "item.all", query = "SELECT i FROM Item i "))
 public class Item extends Rentable {
 
     @ManyToOne
