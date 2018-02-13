0802a8cca9965eeb55a024b93722d5821529e888
zoltanpataki <zolee7777@gmail.com>
2018-02-13 13:24:40 +0100

    commented lines deleted

--- src/main/java/com/codecool/rentsite/user/User.java
+++ src/main/java/com/codecool/rentsite/user/User.java
@@ -21,15 +21,11 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-//    @Column(nullable = false)
     private String firstName;
-//    @Column(nullable = false)
     private String lastName;
     @Column(nullable = false, unique = true)
     private String username;
-//    @Column(nullable = false)
     private String password;
-//    @Column(nullable = false, unique = true)
     private String email;
 
     @OneToMany(mappedBy = "user")
