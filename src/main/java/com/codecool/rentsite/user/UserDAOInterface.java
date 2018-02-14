c7dbbd867e453016edcff3295e94b5b8290d8178
Szűcs Tibor <job.szucstibor@gmail.com>
2018-02-14 09:12:28 +0100

    Add menthod of interface changed to get the first and last name as argument

--- src/main/java/com/codecool/rentsite/user/UserDAOInterface.java
+++ src/main/java/com/codecool/rentsite/user/UserDAOInterface.java
@@ -1,6 +1,6 @@
 package com.codecool.rentsite.user;
 
 public interface UserDAOInterface {
-    void add(String username, String password, String email);
+    void add(String username, String password, String email, String firstName, String lastName);
     User find(int id);
 }
