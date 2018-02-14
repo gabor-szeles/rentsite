a67cfc026f0fee353b2860a9b33d94d137e728be
gabor-szeles <szelesgbr@gmail.com>
2018-02-14 14:27:54 +0100

    getUpdatedData method added

--- src/main/java/com/codecool/rentsite/rentable/RentableService.java
+++ src/main/java/com/codecool/rentsite/rentable/RentableService.java
@@ -1,21 +1,42 @@
 package com.codecool.rentsite.rentable;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.codecool.rentsite.rentable.category.CategoryDAO;
+import com.codecool.rentsite.rentable.category.ItemCategory;
+import com.codecool.rentsite.rentable.category.ServiceCategory;
+
+import java.util.*;
 
 public class RentableService {
 
     private final RentableDAO rentableDAO;
+    private final CategoryDAO categoryDAO;
 
-    public RentableService(RentableDAO rentableDAO) {
+    public RentableService(RentableDAO rentableDAO, CategoryDAO categoryDAO) {
         this.rentableDAO = rentableDAO;
+        this.categoryDAO = categoryDAO;
     }
 
     public Map<String, List<Rentable>> getAllRentables() {
         Map params = new HashMap();
+        List<ItemCategory> itemCategoryList = categoryDAO.getItemCategories();
+        List<ServiceCategory> serviceCategoryList = categoryDAO.getServiceCategories();
         List<Rentable> rentableList = rentableDAO.getAll();
         params.put("rentableList", rentableList);
+        params.put("itemCategories", itemCategoryList);
+        params.put("serviceCategories", serviceCategoryList);
         return params;
+    }
+
+    public List<? extends Rentable> getUpdatedData(String id) {
+        List<? extends Rentable> resultList = new ArrayList<>();
+        String[] idParts = id.replace("\"", "").split("_");
+        String type = idParts[0];
+        int idNumber = Integer.parseInt(idParts[1]);
+        switch (type) {
+            case "item":
+                resultList = rentableDAO.getByItemCategory(idNumber);
+                break;
+        }
+        return resultList;
     }
 }
