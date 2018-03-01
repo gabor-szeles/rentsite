59fa9e922670e8951fcc23590d3e5fb9c16c17fd
Szűcs Tibor <job.szucstibor@gmail.com>
2018-03-01 14:56:23 +0100

    @Table annotation deleted

--- src/main/java/com/codecool/rentsite/review/ReservationReview.java
+++ src/main/java/com/codecool/rentsite/review/ReservationReview.java
@@ -5,7 +5,6 @@ import com.codecool.rentsite.reservation.Reservation;
 import javax.persistence.*;
 
 @Entity
-@Table(name="reservationreview")
 public class ReservationReview extends Review {
 
     @OneToOne
